cad5c5accd8e62c7673f5f4c3780d7834ca3e651
Carrick <lie.carrick1234@gmail.com>
2021-01-16 15:44:00 +0700

    diganti udh ada hash - Bryan and Carrick

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<ctime>
+
+const int MAX_ELEMENT = 10;
+const int MAX_LENGTH = 255;
+char HASHTABLES[MAX_ELEMENT][MAX_LENGTH];
+
+int keisi = -1;
+
+struct Node{
+    char name[255];
+    int index;
+    Node *next,*prev;
+}*head[MAX_ELEMENT], *tail[MAX_ELEMENT];
+
+Node *createNode(const char *name) {
+  Node *newNode = (Node*)malloc(sizeof(Node));
+  strcpy(newNode->name, name);
+  newNode->next = newNode->prev = NULL;
+  return newNode;
+}
+
+int hash(const char *str){
+    return str[0]-'0';
+}
+void inserthashandfriends(const char *str){
+    int index = hash(str);
+    Node *temp = createNode(str);
+    if (index>MAX_ELEMENT){
+        puts("USER IS FULL");
+        return;
+    }
+    if (head[index] == NULL){
+        head[index]=tail[index]=temp;
+    }else if(head[index] != NULL){
+        tail[index]->next = temp;
+        tail[index]->prev = head[index];
+        tail[index] = temp;
+    }
+}
+void viewhash(){
+    for (int i = 0;i<MAX_ELEMENT;i++){
+        if (head[i]){
+            printf("Akun %d: ", i+1);
+            Node *curr = head[i];
+            while(curr){
+                printf("%s <--> ", curr->name);
+                curr = curr->next;
+            }
+            puts(" NULL");
+        }
+    }
+}
+
--- mainfile.cpp
+++ mainfile.cpp
@@ -1,6 +1,15 @@
 #include "Mainmenu.h"
 
 int main(){
-    mainMenu();
+    inserthashandfriends("0kepin");
+    inserthashandfriends("1bryan");
+    inserthashandfriends("2satya");
+    inserthashandfriends("3carrick");
+    inserthashandfriends("4najwa");
+    inserthashandfriends("5shihab");
+    inserthashandfriends("6mampus");;
+    inserthashandfriends("0carrick");//temen kepin ada carrick dan bryan
+    inserthashandfriends("0bryan");
+    viewhash();
     return 0;
 }
